cb06eee35d258fc5a93b79b007aabef625babf47
kalvarez-cr <kalvarez@catreflex.com>
2022-09-19 12:07:59 -0500

    Update axios base url
    - Fix the variable name to use in Next.js

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const useAxios = () => {
-  const requester = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    withCredentials: true,
-    headers: {
-      'content-type': 'application/json',
-    },
-  });
+    const requester = axios.create({
+        baseURL: process.env.NEXT_PUBLIC_APP_BASE_URL,
+        withCredentials: true,
+        headers: {
+            'content-type': 'application/json',
+        },
+    });
 
-  return { requester };
+    return {requester};
 };
