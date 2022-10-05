3e7cdea5defd5a5b7845da21cf7b78af182c373b
kalvarez-cr <kalvarez@catreflex.com>
2022-10-05 15:56:53 -0500

    Update the header logo

--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -47,7 +47,8 @@ const LandingLayout = ({ children }: TLandingLayout) => {
             <>
               <div className="px-2 sm:px-20">
                 <div className="relative flex h-16 items-center justify-between">
-                  <img src="/venvias-logo1.png" alt="logo"/>
+                  <div className="logo-header-bkg"></div>
+                  <img src="/venvias-logo2.png" alt="logo" className="logo-header-img"/>
                   <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     {/* Mobile menu button*/}
                     <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
