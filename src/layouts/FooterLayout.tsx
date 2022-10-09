5f9efd14a71fbc8f177a26695c6f82cda9f94c19
kalvarez-cr <kalvarez@catreflex.com>
2022-10-09 13:27:29 -0500

    Reformat the FooterLayout.tsx

--- src/layouts/FooterLayout.tsx
+++ src/layouts/FooterLayout.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from 'react'
 
-const FooterLayout = ({ children }) => {
-  return (
-    <div className="flex h-screen w-screen flex-col justify-between">
-      {children}
-      <div className="my-2 w-full p-2">
-        <div className="my-4 flex justify-around">
-          {/* <CheckCircleIcon className="mx-4 block h-6 w-6" aria-hidden="true" />
-          <XIcon className="mx-4 block h-6 w-6" aria-hidden="true" /> */}
-          <img src="/fontur.svg" alt="fontur" className="h-14" />
-          <img src="/fontur2.svg" alt="fontur2" className="h-14" />
-          <img src="./fontur3.svg" alt="fontur3" className="h-14" />
+const FooterLayout = ({children}) => {
+    return (
+        <div className="flex h-screen w-screen flex-col justify-between">
+            {children}
+            <div className="my-2 w-full p-2">
+                <div className="my-4 flex justify-around">
+                    <img src="/fontur.svg" alt="fontur" className="h-14"/>
+                    <img src="/fontur2.svg" alt="fontur2" className="h-14"/>
+                    <img src="/fontur3.svg" alt="fontur3" className="h-14"/>
+                </div>
+                <p className="my-2 text-center">Copyright © 2022 Fontur.</p>
+            </div>
         </div>
-        <p className="my-2 text-center">Copyright © 2022 Fontur.</p>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default FooterLayout;
+export default FooterLayout
