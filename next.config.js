87d03a5af9297e5afc2711ff55d3e6d09627074c
ivana <i.alvarez@rsegrp.com>
2023-02-07 15:27:31 -0400

    config for compilation

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-    reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: '/bdv-payment/:id',
-                destination:
-                    'https://biodemo.ex-cle.com:4443/ipg/Web/PaymentProcess/Token/:id',
-                permanent: false,
-                basePath: false,
-            },
-        ];
-    },
-    images: {
-        loader: 'akamai',
-        path: '/',
-    },
+  reactStrictMode: true,
+  basePath: '/app',
+  async redirects() {
+    return [
+      {
+        source: '/bdv-payment/:id',
+        destination:
+          'https://biodemo.ex-cle.com:4443/ipg/Web/PaymentProcess/Token/:id',
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
 };
