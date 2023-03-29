925264186b9ca4068a8a78372116cfea8911f4e6
ivana <iva.alvarez.2@gmail.com>
2023-03-29 13:19:41 -0400

    fix qr image

--- src/pages/qr/[id].tsx
+++ src/pages/qr/[id].tsx
@@ -4,64 +4,54 @@ import { useRouter } from 'next/router';
 import React from 'react';
 
 const qr = () => {
-  const router = useRouter()
-  const { id } = router.query
-  const [base64, setBase64] = React.useState<any>()
+  const router = useRouter();
+  const { id } = router.query;
+  const [base64, setBase64] = React.useState<any>();
 
   const handleClick = async () => {
     const body = {
-      pass_id: id
-    }
+      pass_id: id,
+    };
 
-    
     try {
-        const headers = {
-            'Content-Type': 'application/json',
+      const headers = {
+        'Content-Type': 'application/json',
+      };
+
+      const data = await axios.post(
+        `${process.env.NEXT_PUBLIC_APP_BASE_URL}venpass/qr/`,
+        body,
+        {
+          headers,
         }
-        
-        const data = await axios.post(
-          `${process.env.NEXT_PUBLIC_APP_BASE_URL}venpass/qr/`,
-            body, {
-              headers, 
-              
-            }
+      );
 
-        )
-
-        setBase64(data.data.data.qr_code)
-        
-        
-    } catch (error) {
-
-    }
-}
- React.useEffect(() => {
- handleClick()
-
- },[id])
+      setBase64(data.data.data.qr_code);
+    } catch (error) {}
+  };
+  React.useEffect(() => {
+    handleClick();
+  }, [id]);
   return (
-    <div className="bg-cel  p-12 ">
-      <div className="mb-5 flex w-full items-center justify-center">
+    <>
+    <div className="bg-cel flex flex-col items-center">
+      <div className="mt-7 flex w-full items-center justify-center">
         <Logo className="w-40" />
       </div>
-
-    <p className="text-center font-semibold  text-lg ">
+      <p className="text-center text-lg  font-semibold mt-7">
         Su pago se ha realizado con éxito, puede transitar con su vehículo por
         el peaje, presentando este código QR{' '}
       </p>
-
-      <div className="flex items-center justify-center mt-auto">
+    </div>
+      <div className="flex justify-center">
         <img
           src={`data:image/png;base64,${base64}`}
-          className="w-44 mt-80 mr-20 md:mt-80"
-          alt='qr'
+          className="absolute top-1/2 w-40"
+          alt="qr"
         />
-
-        
       </div>
+    </>
 
-     
-    </div>
   );
 };
 
