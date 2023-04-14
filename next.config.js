cb6996188a8950e2c3b2f4c8b9fdbeb80f57e1fc
ivana <iva.alvarez.2@gmail.com>
2023-04-14 16:15:14 -0400

    delete redirects

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   // basePath: '/app',
-  async redirects() {
-    return [
-      {
-        source: '/bdv-payment/:id',
-        destination:
-          'https://biopago.banvenez.com/IPG/PaymentProcess/Token/:id',
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/bdv-payment/:id',
+  //       destination:
+  //         'https://biopago.banvenez.com/IPG/PaymentProcess/Token/:id',
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
   images: {
     loader: 'akamai',
     path: '/',
