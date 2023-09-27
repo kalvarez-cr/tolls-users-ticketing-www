0424489ab7eea84c6290651b6fad8d128705913e
Isabella Noy <isabellaalenyr@gmail.com>
2023-09-27 11:59:50 -0400

    Recharge graph

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,64 +1,29 @@
 import React, { PureComponent } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { UseApiCall } from 'hooks/useApiCall';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+
+
 
 const BarChartComponent = () => {
- 
+  const { useGet } = UseApiCall();
+  const { data, isLoading } = useGet({
+    queryKey: 'getBarChart',
+    url: '/dashboard/transit_recharge_graph/',
+  });
 
    
     return (
       
         <>
+        {!isLoading ? 
+        
         <div className='flex flex-col p-6 overflow-x-scroll rounded-xl shadow-md bg-[#fff] bg-opacity-100 '>
         <p className='mb-6 text-gray-500 text-center'>Recarga vs consumo</p>
         <BarChart
           width={600}
           height={250}
-          data={data}
+          data={data?.data?.data}
           margin={{
             top: 5,
             right: 30,
@@ -67,14 +32,27 @@ const BarChartComponent = () => {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <XAxis dataKey="x_axis" />
+          <YAxis dataKey="recharge" />
           <Tooltip />
           <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
+          <Bar dataKey="recharge" fill="#8884d8" />
+         
         </BarChart>
         </div>
+        :
+          
+<div className="flex flex-col p-6 rounded-xl shadow-md bg-white ">
+          <h2 className="mb-6 text-lg text-black font-bold">Recarga vs consumo</h2>
+       
+          
+          <div className="flex flex-row justify-center my-12">
+            <div className="w-80 h-80  bg-gray-300  animate-pulse" />
+          </div>
+          </div>
+
+        
+        }
         </>
      
     );
